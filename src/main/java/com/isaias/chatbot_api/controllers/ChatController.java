82bf996fe7a948edb42d08c4110efdfa0ba763a8
isaiasgj0002 <isaiasguzman863@gmail.com>
2023-08-25 19:54:50 -0500

    mejoras

--- src/main/java/com/isaias/chatbot_api/controllers/ChatController.java
+++ src/main/java/com/isaias/chatbot_api/controllers/ChatController.java
@@ -22,10 +22,39 @@ public class ChatController {
     private String processMessage(String message) {
         // Implementa la lógica para procesar el mensaje y generar una respuesta
         // Puedes usar condiciones, bases de datos, algoritmos de procesamiento de lenguaje natural, etc.
-        if (message.equalsIgnoreCase("Hola")) {
+        if (message.toLowerCase().contains("hola") ||
+            message.toLowerCase().contains("buenos") ||
+            message.toLowerCase().contains("buenas")) {
             return "¡Hola! ¿En qué puedo ayudarte?";
-        } else if (message.equalsIgnoreCase("Cómo estás")) {
+        } else if (message.toLowerCase().contains("cómo estás")) {
             return "Estoy bien, gracias por preguntar.";
+        } else if (message.toLowerCase().contains("información")) {
+            return "Somos una empresa dedicada al desarrollo de software. Realizamos aplicaciones móviles, sitios web y sistemas para escritorio según tus necesidades.";
+        }else if(message.toLowerCase().contains("horario")){
+            return "Atendemos desde las 9:00am hasta las 8:00pm";
+        }else if(message.toLowerCase().contains("dirección")||
+                 message.toLowerCase().contains("ubicación")||
+                 message.toLowerCase().contains("localización")
+                ){
+            return "Puedes encontrarnós en la calle oswaldo baca 201 en la urb magisterio 1era etapa, Cusco";
+        } else if (message.toLowerCase().contains("servicios")) {
+            return "Ofrecemos servicios de desarrollo de aplicaciones móviles, diseño de sitios web, desarrollo de sistemas personalizados y consultoría en tecnología.";
+        } else if (message.toLowerCase().contains("tecnologías") || 
+                   message.toLowerCase().contains("lenguajes")) {
+            return "Trabajamos con una variedad de tecnologías y lenguajes de programación, incluyendo Java, Python, JavaScript, React, Angular, y más.";
+        } else if (message.toLowerCase().contains("casos de éxito")) {
+            return "Hemos trabajado en diversos proyectos exitosos, incluyendo aplicaciones móviles para negocios locales, sistemas de gestión para empresas y más.";
+        } else if (message.toLowerCase().contains("equipo") || 
+                   message.toLowerCase().contains("desarrolladores")) {
+            return "Nuestro equipo está compuesto por profesionales altamente calificados y apasionados por la tecnología.";
+        } else if (message.toLowerCase().contains("cotización") ||
+                   message.toLowerCase().contains("presupuesto")) {
+            return "Para obtener una cotización personalizada, te invitamos a ponerte en contacto con nosotros.";
+        } else if (message.toLowerCase().contains("contacto") || 
+                   message.toLowerCase().contains("asesor")) {
+            return "Puedes contactarte con un asesor especializado escribiendo al +51 959 676 443 o enviando un correo a info@twoplotech.online.";
+        }else if (message.toLowerCase().contains("gracias")) {
+            return "De nada, no olvides ponerte en contacto si tienes mas dudas";
         } else {
             return "Lo siento, no entiendo tu pregunta.";
         }
